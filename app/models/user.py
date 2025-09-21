ce05a83b073727093332ab82288780d7c4941ed5
roridemonslay <olaniyideborah63@gmail.com>
2025-09-21 19:59:35 -0400

    the database working

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean, func, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Mapped, relationship
+from typing import List
 
 #all this is just doing is connection all of the stuff from our databse 
 Base = declarative_base()
@@ -12,10 +13,11 @@ class User(Base):
     insta_password = Column(String(100), nullable = False)
     is_active = Column(Boolean, default = True, nullable = False)
     created_at = Column(DateTime, default=func.now())
+    relationship: Mapped[List["FollowRelationship"]] = relationship(back_populates = "user") #we're using list because we're tracking many users under the follow relaitonship
 class FollowRelationship(Base):
     __tablename__ ="follower_relationships"
     id = Column(Integer, primary_key = True)
-    users_id = Column(Integer, ForeignKey("users.id")) #this just the link to the user table
+    user_id = Column(Integer, ForeignKey("users.id")) #this just the link to the user table, allows you to return not just the table number but assigned the id to the user 
     instagram_user_id = (Column(String(500), nullable = False))
     full_name = (Column(String(100))) #user full name 
     profile_pic_url = (Column(String(300))) # profile flics
@@ -23,3 +25,7 @@ class FollowRelationship(Base):
     i_follow_them = Column(Boolean, default = True)
     they_follow_me= Column(Boolean,default = False ) #this could be either true of false depending 
     last_checked = Column(DateTime,default = func.now())
+    user : Mapped["User"] = relationship(back_populates = "relationship")
+    #note that when using back_poulates, conect it to the attrivuate not the class. 
+#want to go from follower relationship to user 
+#and user relaitosnhip to user  but we cna just add mapping to the two classes we have above
