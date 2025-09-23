5a744430962a0c7053a3b0bf2e946adece55b32d
roridemonslay <olaniyideborah63@gmail.com>
2025-09-23 13:17:32 -0400

    insta_scraper

--- app/services/instagram_scraper.py
+++ app/services/instagram_scraper.py
@@ -3,4 +3,9 @@ class InstagramScraper:
     def __init__(self):
         #t6jtis creates a session to keep cookies (data) between sessions     
         self.session = requests.Session()
-        
+
+        self.session.headers.update({
+            'Uset-Agent' :  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+        })
+        self.is_logged_in = False
+
