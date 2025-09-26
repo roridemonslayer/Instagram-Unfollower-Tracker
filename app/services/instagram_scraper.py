4e69536b31eadb884e4c28fda7fd9e9f0840b23a
roridemonslay <olaniyideborah63@gmail.com>
2025-09-25 21:50:09 -0400

    insta_scraper

--- app/services/instagram_scraper.py
+++ app/services/instagram_scraper.py
@@ -1,11 +1,36 @@
 import requests #using this to get HTTP requests to post, get etc our data
 class InstagramScraper: 
     def __init__(self):
-        #t6jtis creates a session to keep cookies (data) between sessions     
+        #t6jtis creates a session to keep cookies (data) between sessions
+        #and to make requests   
         self.session = requests.Session()
+        self.is_logged_in = False #this js like ios saying the user hasn't logged in
+    def login(self, username, password):
+        print(f"trying to login as {username}")
+        #we use self.session.get to keep cookies between requests. 
+        login_information = self.session.get("https://www.instagram.com/accounts/login/")
 
-        self.session.headers.update({
-            'Uset-Agent' :  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-        })
-        self.is_logged_in = False
+#how do i check if a request was succesfull
+ #what are http status codes?
+ #how i access the status cdoe form my request 
+        if login_information.status_code== 200:
+            #so if it runs and the session wroks to get the user info we get 200 and return True
+            print("It works")
+            page_content = login_information.text #this is being used to get the HTML token cotent
+            #csrf is cross-site-requestt-forgery-protetction . it stops hackers fromjust trying mutliple insta users and passwords to get into the website 
+            #they'd need to make it much harder for hackers
 
+        if "csrf" in page_content: 
+            print("found it")
+            csrf_start = page_content.find("csrf")
+            print("it appears around:" , csrf_start)
+            print(page_content[csrf_start-50:csrf_start+150])
+        else:
+            print("No CSRF token found")
+            print(page_content[:2000]) #prints the firtst 500 characters of whats there.
+            
+
+
+if __name__ == "__main__":
+    scraper = InstagramScraper()
+    scraper.login("test_username", "test_password")
