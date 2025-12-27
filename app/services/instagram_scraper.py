7db856cdd09900d90203c3d062916552f566568c
roridemonslay <olaniyideborah63@gmail.com>
2025-12-26 22:20:23 -0500

    this is the added script to get the user following

--- app/services/instagram_scraper.py
+++ app/services/instagram_scraper.py
@@ -29,8 +29,8 @@ class InstagramScraper:
             login_button = self.driver.find_element(By.XPATH, "//button[@type='submit']")
             login_button.click()
             #wait for the login to compelete
-            WebDriverWait(self.diver, 15).until(
-                EC.url_change("https://www.instagram.com/accounts/login/")
+            WebDriverWait(self.driver, 15).until(
+                EC.url_changes(f"https://www.instagram.com/{username}/")
             )
 
             #this then checks if we're logged in by looking at the url 
@@ -39,13 +39,40 @@ class InstagramScraper:
                 self.is_logged_in = True
             else:
                 print("login failed- recheck user/pass")
-                self.is_logged_in = True 
+                self.is_logged_in = False 
         except Exception as e:
             print(f"Error During login: {e}")
             self.is_logged_in = False
 
+
+
+
+
+    def get_following(self, username):
+        """ get the list of the users following"""
+        print(f" Get the follower person username: {username}")
+
+        """ we then need a place to navigate to get the user following"""
+        self.driver.get(f"https://www.instagram.com/accounts/{username}/")
+
+        time.sleep(3) 
+
+        #task: Find the "following" link and click it
+        try: 
+            following = WebDriverWait(self.driver, 15).until(
+                EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "following"))
+            )
+
+            print(f"Found the elements in follow: {following}")
+            following.click()
+
+        except Exception as e: 
+            print(f"An error occurred: {e}")
+
 if __name__ == "__main__":
     scraper = InstagramScraper()
-    scraper.login("USERNAME","PASSWORD")
+   
+    scraper.login("username","pass")
     input("Press Enter to close browser...")
+
     scraper.driver.quit()
