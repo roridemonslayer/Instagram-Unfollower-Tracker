14e7365d2432f15be21cc8f28dcbb7de0366aa6d
roridemonslay <olaniyideborah63@gmail.com>
2026-01-04 21:42:18 -0500

    working scraper for the follweors and unfollowers

--- app/services/instagram_scraper.py
+++ app/services/instagram_scraper.py
@@ -1,78 +1,277 @@
-from selenium import webdriver #we're going to use selenium as an automator here that way insta doesn't detect us as a bot
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.ui import WebDriverWait 
+from selenium import webdriver  # we're going to use selenium as an automator here that way insta doesn't detect us as a bot
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+
 class InstagramScraper: 
     def __init__(self):
-        #t6jtis creates a session to keep cookies (data) between sessions
-        #and to make requests   
+        # Creates a Chrome browser instance that Selenium will control
+        # This keeps cookies between requests so Instagram remembers we're logged in
         self.driver = webdriver.Chrome()
-        self.is_logged_in = False #this js like ios saying the user hasn't logged in
+        self.is_logged_in = False # Tracks whether the user has successfully logged in
+    
     def login(self, username, password):
         print(f"trying to login as {username}")
-        #we use self.session.get to keep cookies between requests. 
+        # Navigate to Instagram's login page
         self.driver.get("https://www.instagram.com/accounts/login/")
         
-   
-
         try:
+            # Wait up to 15 seconds for the username input box to appear on the page
             username_input = WebDriverWait(self.driver, 15).until(
-                EC.presence_of_element_located((By.NAME, "username" ))
+                EC.presence_of_element_located((By.NAME, "username"))
             )
-            username_input.send_keys(username) #this types the username
+            # Type the username into the input box
+            username_input.send_keys(username)
 
+            # Find the password input box and type the password
             password_input = self.driver.find_element(By.NAME, "password")
             password_input.send_keys(password)
 
-            #next it needs to the the login vbutton to click 
+            # Find the submit button and click it to log in
             login_button = self.driver.find_element(By.XPATH, "//button[@type='submit']")
             login_button.click()
-            #wait for the login to compelete
-            WebDriverWait(self.driver, 15).until(
-                EC.url_changes(f"https://www.instagram.com/{username}/")
-            )
-
-            #this then checks if we're logged in by looking at the url 
+            
+            # Wait 10 seconds for Instagram to process the login
+            time.sleep(10)
+            
+            # Instagram shows popups after login - dismiss them
+            # Try to click "Not now" on the "Save Login Info" popup
+            try:
+                not_now_button = self.driver.find_element(By.XPATH, "//button[contains(text(), 'Not now') or contains(text(), 'Not Now')]")
+                not_now_button.click()
+                time.sleep(2)
+            except:
+                pass  # If popup doesn't appear, that's fine - just continue
+            
+            # Try to dismiss the notifications popup if it appears
+            try:
+                not_now_button = self.driver.find_element(By.XPATH, "//button[contains(text(), 'Not now') or contains(text(), 'Not Now')]")
+                not_now_button.click()
+                time.sleep(2)
+            except:
+                pass  # If popup doesn't appear, that's fine
+            
+            print(f"After login, current URL: {self.driver.current_url}")
+            
+            # Check if we successfully logged in by seeing if we left the login page
             if "accounts/login" not in self.driver.current_url:
                 print("login works")
                 self.is_logged_in = True
             else:
-                print("login failed- recheck user/pass")
-                self.is_logged_in = False 
+                print("login failed")
+                self.is_logged_in = False
+                
         except Exception as e:
             print(f"Error During login: {e}")
             self.is_logged_in = False
 
-
-
-
-
     def get_following(self, username):
-        """ get the list of the users following"""
-        print(f" Get the follower person username: {username}")
+        """Gets the list of accounts that the user is following"""
+        print(f"Get the follower person username: {username}")
 
-        """ we then need a place to navigate to get the user following"""
+        # Navigate to the user's Instagram profile page
         self.driver.get(f"https://www.instagram.com/{username}/")
-
+        
+        # Wait 3 seconds for the profile page to load
         time.sleep(3) 
 
-        #task: Find the "following" link and click it
-        try: 
+        try:
+            # Find the "following" link on the profile and wait until it's clickable
+            # The link has '/following' in its href attribute
             following = WebDriverWait(self.driver, 15).until(
-                EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "following"))
+                EC.element_to_be_clickable((By.XPATH, "//a[contains(@href, '/following')]"))
             )
-
-            print(f"Found the elements in follow: {following}")
+            print(f"Found following link")
+            
+            # Click the following link to open the popup
             following.click()
+            print("Clicked following!")
+            
+            # Wait 5 seconds for the following popup/modal to appear
+            time.sleep(5)
+            
+            print("Starting to scrape usernames...")
+            
+            # Find the scrollable container inside the popup
+            # Instagram puts following list in a div with 'overflow' style
+            scroll_box = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//div[@role='dialog']//div[contains(@style, 'overflow')]"))
+            )
+            
+            # Get the initial height of the scrollable area
+            # We'll use this to know when we've reached the bottom
+            last_height = self.driver.execute_script("return arguments[0].scrollHeight", scroll_box)
+            
+            # Scroll through the popup to load all users
+            # Instagram lazy-loads users as you scroll, so we need to scroll to see everyone
+            for i in range(10):  # Scroll up to 10 times
+                # Scroll to the bottom of the container
+                self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scroll_box)
+                
+                # Wait 2 seconds for Instagram to load more users
+                time.sleep(2)
+                
+                # Check the new height after scrolling
+                new_height = self.driver.execute_script("return arguments[0].scrollHeight", scroll_box)
+                
+                # If height didn't change, we've reached the end - stop scrolling
+                if new_height == last_height:
+                    break
+                    
+                # Update last_height for next iteration
+                last_height = new_height
+            
+            # Now extract all the usernames from the popup
+            # Find all links (a tags) that have an href attribute
+            user_elements = scroll_box.find_elements(By.XPATH, ".//a[contains(@href, '/')]")
+            
+            # Create an empty list to store usernames
+            following_list = []
+            
+            # Loop through each link we found
+            for user in user_elements:
+                # Get the href attribute (the URL)
+                href = user.get_attribute("href")
+                
+                # Check if href exists and has enough slashes (valid profile link)
+                # Valid profile links look like: https://instagram.com/username/
+                if href and href.count("/") >= 4:
+                    # Split the URL by "/" and get the username (second to last part)
+                    username = href.split("/")[-2]
+                    
+                    # Only add if username exists and isn't already in our list (no duplicates)
+                    if username and username not in following_list:
+                        following_list.append(username)
+            
+            # Print results
+            print(f"\nFound {len(following_list)} users you're following:")
+            # Print first 10 usernames as a preview
+            for user in following_list[:10]:
+                print(f"  - {user}")
+            
+            # Return the complete list of usernames
+            return following_list
 
-        except Exception as e: 
+        except Exception as e:
+            # If anything goes wrong, print the error and take a screenshot for debugging
             print(f"An error occurred: {e}")
+            self.driver.save_screenshot("debug.png")
+            return []  # Return empty list if there was an error
+        
+    def get_followers(self, username):
+        #this is the function we're using to pull the users followers 
+        print(f"Get the users followers: {username}")
+        self.driver.get(f"https://www.instagram.com/{username}/")
+        time.sleep(3) 
 
+        try:
+            # Find the "following" link on the profile and wait until it's clickable
+            # The link has '/following' in its href attribute
+            following = WebDriverWait(self.driver, 15).until(
+                EC.element_to_be_clickable((By.XPATH, "//a[contains(@href, '/followers')]"))
+            )
+            print(f"Found follower link")
+            
+            # Click the following link to open the popup
+            following.click()
+            print("Clicked follower!")
+            
+            # Wait 5 seconds for the following popup/modal to appear
+            time.sleep(5)
+            
+            print("Starting to scrape usernames...")
+            
+            # Find the scrollable container inside the popup
+            # Instagram puts following list in a div with 'overflow' style
+            scroll_box = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//div[@role='dialog']//div[contains(@style, 'overflow')]"))
+            )
+            
+            # Get the initial height of the scrollable area
+            # We'll use this to know when we've reached the bottom
+            last_height = self.driver.execute_script("return arguments[0].scrollHeight", scroll_box)
+            
+            # Scroll through the popup to load all users
+            # Instagram lazy-loads users as you scroll, so we need to scroll to see everyone
+            for i in range(10):  # Scroll up to 10 times
+                # Scroll to the bottom of the container
+                self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scroll_box)
+                
+                # Wait 2 seconds for Instagram to load more users
+                time.sleep(2)
+                
+                # Check the new height after scrolling
+                new_height = self.driver.execute_script("return arguments[0].scrollHeight", scroll_box)
+                
+                # If height didn't change, we've reached the end - stop scrolling
+                if new_height == last_height:
+                    break
+                    
+                # Update last_height for next iteration
+                last_height = new_height
+            
+            # Now extract all the usernames from the popup
+            # Find all links (a tags) that have an href attribute
+            user_elements = scroll_box.find_elements(By.XPATH, ".//a[contains(@href, '/')]")
+            
+            # Create an empty list to store usernames
+            following_list = []
+            
+            # Loop through each link we found
+            for user in user_elements:
+                # Get the href attribute (the URL)
+                href = user.get_attribute("href")
+                
+                # Check if href exists and has enough slashes (valid profile link)
+                # Valid profile links look like: https://instagram.com/username/
+                if href and href.count("/") >= 4:
+                    # Split the URL by "/" and get the username (second to last part)
+                    username = href.split("/")[-2]
+                    
+                    # Only add if username exists and isn't already in our list (no duplicates)
+                    if username and username not in following_list:
+                        following_list.append(username)
+            
+            # Print results
+            print(f"\nFound {len(following_list)} users you're following:")
+            # Print first 10 usernames as a preview
+            for user in following_list[:10]:
+                print(f"  - {user}")
+            
+            # Return the complete list of usernames
+            return following_list
+
+        except Exception as e:
+            # If anything goes wrong, print the error and take a screenshot for debugging
+            print(f"An error occurred: {e}")
+            self.driver.save_screenshot("debug.png")
+            return []  # Return empty list if there was an error
+        
+
+
+
+
+
+
+
+
+
+
+# This code runs when you execute the script
 if __name__ == "__main__":
+    # Create an instance of InstagramScraper
     scraper = InstagramScraper()
+    
+    # Log in with your credentials
     scraper.login("theoneandonly3034","roriolaniyi123")
-    # You need to add this line:
+    
+    # Get the list of people you're following
     scraper.get_following("theoneandonly3034")
+    scraper.get_followers("theoneandonly3034")
+    
+    # Keep the browser open until you press Enter
     input("Press Enter to close browser...")
+    
+    # Close the browser
     scraper.driver.quit()
